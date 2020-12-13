3f84d4a35d7fb34c84aad6a6e9f7aa67d8e522bb
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2020-12-12 18:43:28 -0600

    Create Desarrolladores.kt

---
+++ app/src/main/java/com/example/proyectoubicacion/Activities/Desarrolladores.kt
@@ -0,0 +1,12 @@
+package com.example.proyectoubicacion.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyectoubicacion.R
+
+class Desarrolladores : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_desarrolladores)
+    }
+}
