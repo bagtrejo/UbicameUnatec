039f2518c782fd1e4b03a5d83524f80260204d51
bagtrejo <55471155+bagtrejo@users.noreply.github.com>
2020-12-13 23:34:29 -0600

    Update ListElement.java

--- app/src/main/java/com/example/proyectoubicacion/Activities/ListElement.java
+++ app/src/main/java/com/example/proyectoubicacion/Activities/ListElement.java
@@ -4,11 +4,15 @@ public class ListElement {
     public String titulo;
     public String Descripcion;
     public int imagen;
+    public int imagen2;
+    public int imagen3;
 
-    public ListElement(String titulo, String descripcion, int imagen) {
+    public ListElement(String titulo, String descripcion, int imagen, int imagen2, int imagen3) {
         this.titulo = titulo;
         Descripcion = descripcion;
         this.imagen = imagen;
+        this.imagen2 = imagen2;
+        this.imagen3 = imagen3;
     }
 
     public String getTitulo() {
@@ -33,5 +37,21 @@ public class ListElement {
 
     public void setImagen(int imagen) {
         this.imagen = imagen;
+    }
+
+    public int getImagen2() {
+        return imagen2;
+    }
+
+    public void setImagen2(int imagen2) {
+        this.imagen2 = imagen2;
+    }
+
+    public int getImagen3() {
+        return imagen3;
+    }
+
+    public void setImagen3(int imagen3) {
+        this.imagen3 = imagen3;
     }
 }
