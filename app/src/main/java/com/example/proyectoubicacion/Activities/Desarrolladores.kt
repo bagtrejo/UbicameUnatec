2fbf4ebde468dc29d1dee78ad55fe71818b99ab9
bagtrejo <55471155+bagtrejo@users.noreply.github.com>
2020-12-15 14:18:46 -0600

    Update Desarrolladores.kt

--- app/src/main/java/com/example/proyectoubicacion/Activities/Desarrolladores.kt
+++ app/src/main/java/com/example/proyectoubicacion/Activities/Desarrolladores.kt
@@ -3,10 +3,14 @@ package com.example.proyectoubicacion.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.proyectoubicacion.R
+import com.google.android.material.appbar.MaterialToolbar
 
 class Desarrolladores : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_desarrolladores)
+
     }
 }
